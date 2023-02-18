a232d02cded94868064dff524f92377ca4b617e0
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-02-18 13:10:57 -0700

    Add new item deliverable

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,7 +3,7 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [search, setSearch] = useState("");
 
@@ -17,7 +17,8 @@ function ShoppingList({ items }) {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm 
+      onItemFormSubmit={onItemFormSubmit}/>
       <Filter 
       onCategoryChange={handleCategoryChange} 
       search={search}
