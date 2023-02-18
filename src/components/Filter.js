1c66abff3e61ab24cbd73ebe161095d7852f7372
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-02-18 13:00:14 -0700

    Add filter functionality and state

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, search, onSearchChange }) {
+
+  function handleSearchChange(event) {
+    onSearchChange(event.target.value)
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input 
+      type="text" 
+      name="search" 
+      placeholder="Search..."
+      value={search} 
+      onChange={handleSearchChange}
+      />
+      <select 
+      name="filter" 
+      onChange={onCategoryChange}
+      >
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,24 +5,30 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items
+  .filter((item) => selectedCategory === "All" ? true : item.category === selectedCategory)
+  .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+      onCategoryChange={handleCategoryChange} 
+      search={search}
+      onSearchChange={setSearch}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          <Item 
+          key={item.id} 
+          name={item.name} 
+          category={item.category}
+           />
         ))}
       </ul>
     </div>
